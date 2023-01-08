7c5b2acd8e62e6a1c7aae4247601a61e429c8086
chaimaaSafi <safichaimaa098@gmail.com>
2023-01-09 00:21:41 +0100

    feat: add nnext seo config

---
+++ config/next-seo.config.ts
@@ -0,0 +1,21 @@
+const SEO = {
+  defaultTitle: "dayDhikr",
+  titleTemplate: "%s",
+  description: "And whoever is with God, everyone is with him",
+
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://www.daydhikr.us/",
+    site_name: "daydhikr",
+    images: [
+      {
+        url: "https://www.daydhikr.us/assets/logo.png",
+        height: 1142,
+        width: 1418,
+      },
+    ],
+  },
+};
+
+export default SEO;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,15 @@
 import type { AppProps } from "next/app";
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/styles/globals.css";
+import { DefaultSeo } from "next-seo";
+import SEO from "../../config/next-seo.config";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
